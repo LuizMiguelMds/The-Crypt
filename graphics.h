0a441bb6b32ab8da2a5eb7f64971c229c6e897df
Luiz Miguel <luizmiguelmds2004@gmail.com>
2024-12-19 20:43:28 -0300

    Listagem de mais funções e codigo comentado

--- graphics.h
+++ graphics.h
@@ -1,6 +1,14 @@
 //cabeçarios do display/interface do jogo
 
+//lista das cores do texto
 enum COLORS;
+//lista das cores do fundo
 enum BACKGROUND COLORS;
+//define as cores do texto
 void textColor(int letras, int fundo);
+//define as cores do texto de fundo
 void resetColor();
+//funcao para movimentacao de texto no terminal
+void linhaCol(int linha, int col);
+//funcao para a criacao da box grafica
+void box(int lin1, int col1, int lin2, col2);
