9e69f230a62d683a7786546094ed9e5cdb9bc0a3
Luiz Miguel <luizmiguelmds2004@gmail.com>
2024-12-19 21:02:00 -0300

    Criação da função linhaCol

--- graphics.c
+++ graphics.c
@@ -1,3 +1,10 @@
 //arquivo.c do graphics.h
 
 #include "graphics.h"
+
+void linhaCol(int lin, int col) {
+    COORD coord;
+    coord.X = col - 1;
+    coord.Y = lin - 1;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
