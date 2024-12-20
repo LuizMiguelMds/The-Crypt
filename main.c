ba5a54400435747f6452ab97c530d5264fd13f43
Luiz Miguel <luizmiguelmds2004@gmail.com>
2024-12-20 01:18:18 -0300

    Inserção do menu Principal

--- main.c
+++ main.c
@@ -3,7 +3,34 @@
 #include "graphics.h"
 
 int main() {
-	system("chcp 437 > nul");
-    theCrypt();
+    int opc;
+    char lista[4][40] = {"Novo Jogo", "Carregar Jogo", "Configs", "Sair/Encerrar programa"};
+    
+    system("chcp 437 > nul");
+    
+    while (1) {
+        theCrypt(); /* Exibe o cabecalho apenas uma vez por loop */
+        opc = menuPrincipal(15, 4, 4, lista); /* Exibe o menu principal */
+
+        switch (opc) {
+            case 1:
+                resetColor();
+                break;
+            case 2:
+                resetColor();
+                break;
+            case 3:
+                resetColor();
+                break;
+            case 4:
+                linhaCol(30, 25);
+                printf("Encerrando o programa...\n");
+                resetColor();
+                exit(0);
+                break;
+        }
+    }
+    resetColor();
     return 0;
 }
+
