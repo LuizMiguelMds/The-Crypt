a224687de88da3e125ba54983089c00f0130b57a
Luiz Miguel <luizmiguelmds2004@gmail.com>
2024-12-19 21:07:33 -0300

    Criação da função resetColor

--- graphics.c
+++ graphics.c
@@ -6,6 +6,10 @@ void TextColor(int letras, int fundo) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), letras + fundo);
 }
 
+void ResetColor() {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), WHITE | _BLACK);
+}
+
 void linhaCol(int lin, int col) {
     COORD coord;
     coord.X = col - 1;
